36c58c7dc00a15927f7c0acb463ff961b66ca4c6
chivaachino <art_morelia@hotmail.com>
2015-04-09 21:29:16 -0500

    Create WSQ08.py

---
+++ WSQ08.py
@@ -0,0 +1,24 @@
+def sum(num1,num2): 
+    print ("La suma es: ", x+y)
+ 
+def dif(num1,num2):
+    print ("La resta es: ", x-y)
+ 
+def pro(num1,num2):
+    print ("La multiplicacion es: ", x*y)
+ 
+def div(num1,num2):
+    print ("La divicion es: ", x/y)
+ 
+def rem(num1,num2):
+    print ("El residuo es: ", x%y)
+ 
+ 
+num1 = int(input("Dame el primer numero: "))
+num2= int(input("Dame el segundo numbero: "))
+
+sum(num1,num2)
+dif(num1,num2)
+pro(num1,num2)
+div(num1,num2)
+rem(num1,num2)
