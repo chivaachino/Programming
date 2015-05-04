da3cfe4f69c5be7104c3980caeabf29707b75318
chivaachino <art_morelia@hotmail.com>
2015-05-04 11:53:24 -0500

    Update Quiz10_1.py

--- Quiz10_1.py
+++ Quiz10_1.py
@@ -1,11 +1,11 @@
-def find_threes(lista): #funcion para crear la nueva lista con elementos divisibles entre 3
- n_lista=[]
+def find_threes(lis): #funcion para crear la nueva lista con elementos divisibles entre 3
+ n_lis=[]
  for num in lista:
-  if (num%3 ==0 ):
-   n_lista.append(num)
- return n_lista
+  if (num%3 == 0 ):
+   n_lis.append(num)
+ return n_lis
 
-lista=[] #nueva lista
+lis=[] #nueva lista
 ans="si"
 
 print("Crearemos una lista.")
@@ -13,19 +13,19 @@ print("\n")
 
 while ans=="si": #Elementos de la lista
  num=int(input(" Introduce un valor -> "))
- lista.append(num)
- cant=len(lista)
+ lis.append(num)
+ cant=len(lis)
  print("tienes ",cant," Valores en la lista")
  ans=input(str("Quieres agregar otro numero? (si/no) -> "))
  print("\n")
 
  #llamado de funciones
-find_threes(lista)
-lista_divisible=(find_threes(lista))
-suma_divisibles=sum(find_threes(lista))
+find_threes(lis)
+lista_divisible=(find_threes(lis))
+suma_divisibles=sum(find_threes(lis))
 
 #salidas
 print("\n")
-print("La lista, ",lista, "tiene ",cant," valores.")
+print("La lista, ",lis, "tiene ",cant," valores.")
 print(lista_divisible," son divisibles entre 3")
 print("el resultado de la suma de estos numeros es de ",suma_divisibles)
